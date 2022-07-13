722e6b80a6777a111d33eb6184acba46199f065d
Kalyan <kalyanrko14@gmail.com>
2022-07-13 12:49:13 +0530

    loanApi

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const mongoose = require("./db/connection/connect");
+var loanroute = require("./Routes/loanroutes");
+var KnexSessionStore = require("connect-session-knex");
 
-const url =
-  "mongodb+srv://batman:Batman@718@mymern.f4pjnwv.mongodb.net/?retryWrites=true&w=majority";
+const store = require("./utils/sessionstore");
 
-const client = new MongoClient(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  console.log("inside the cors middleware");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader("Cache-Control", "no-cache");
+  next();
 });
-client.connect((error) => {
-  if (!error) {
-    console.log("sucess");
-  } else console.log(error);
-});
+
+app.use(
+  session({
+    store: store,
+    secret: "bat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: false,
+      secure: false,
+      maxAge: 1000 * 60 * 5,
+    },
+  })
+);
+
+app.use("/", loanroute);
 
 app.listen(8000, function () {
   console.log("server started");
