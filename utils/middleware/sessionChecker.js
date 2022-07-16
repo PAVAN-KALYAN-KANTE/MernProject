626fc9f4570ed0a417bd11132c00fde2d19bcd2f
Kalyan <kalyanrko14@gmail.com>
2022-07-16 19:14:20 +0530

    backend

--- utils/middleware/sessionChecker.js
+++ utils/middleware/sessionChecker.js
@@ -3,7 +3,6 @@ var sessionChecker = (request, response, next) => {
   console.log(request.body);
   console.log("req.body.sessionId inside middleware", request.body.sessionId);
   if (request.method === "POST") {
-    console.log("inside if");
     store.get(request.body.sessionId, (err, session) => {
       if (err) {
         console.log("err");
